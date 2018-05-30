b2c5269fd8d5f53564983d65df557473f210c335
braincodercn <braincodercn@aliyun.com>
2018-05-30 10:31:47 +0800

    update run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python fetch_papers.py
+python download_pdfs.py
+python parse_pdf_to_text.py
+python thumb_pdf.py
+python analyze.py
+python buildsvm.py
+python make_cache.py
+
+
+python serve.py --prod --port 80
